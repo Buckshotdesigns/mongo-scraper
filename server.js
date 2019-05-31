5548bee65fc9efd611f811bc560bd445a0684713
bryanbuckus <bryanbuckus@gmail.com>
2019-05-31 15:13:36 -0400

    initial setup working

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+  require("./config/routes")(app);
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+app.listen(PORT, function(){
+    console.log("Listening on port" + PORT);
+});
+module.exports = app;
